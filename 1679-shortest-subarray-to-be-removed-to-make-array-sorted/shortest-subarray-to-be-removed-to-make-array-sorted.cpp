98898f9a7f090bdec3e15312cab35b7d4efc2cee
Aditya Shukla <142577439+AdityaShuk1a@users.noreply.github.com>
2024-11-15 20:06:03 +0530

    Time: 0 ms (100.00%) | Memory: 69.7 MB (25.81%) - LeetSync

---
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& arr) {
+        int r = arr.size()-1;
+        int l = 0;
+        while(r > 0 && arr[r] >= arr[r-1]){
+            r--;
+        }
+
+        if(r == 0){
+            return 0;
+        }
+        int minn = r;
+        while(l < r && (l == 0 || arr[l] >= arr[l-1])){
+            while(r < arr.size() && arr[l] > arr[r]){
+                r++;
+            }
+
+            minn =  min(minn, r-l-1);
+            l++;
+        }
+
+        return minn;
+    }
+};
