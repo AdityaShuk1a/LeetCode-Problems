689ac9722a4db4a2f056e8773820b24eb3a93ffc
Aditya Shukla <142577439+AdityaShuk1a@users.noreply.github.com>
2026-01-23 14:14:53 +0530

    Time: 2149 ms (5.00%) | Memory: 48.4 MB (6.23%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,22 +1,58 @@
 class Solution {
 
-    public boolean check(int[] nums, int idx, boolean[] visited){
-        if(idx >= nums.length || idx < 0 || visited[idx]) return false;
+    // public boolean check(int[] nums, int idx, boolean[] visited){
+    //     if(idx >= nums.length || idx < 0 || visited[idx]) return false;
         
-        if(idx == nums.length - 1) return true;
+    //     if(idx == nums.length - 1) return true;
 
-        visited[idx] = true;
+    //     visited[idx] = true;
 
-        int maxJump = nums[idx];
-        for(int i = 1; i <= maxJump; i++){
-            if(check(nums, idx + i, visited)) return true;
-        }
+    //     int maxJump = nums[idx];
+    //     for(int i = 1; i <= maxJump; i++){
+    //         if(check(nums, idx + i, visited)) return true;
+    //     }
 
-        return false; 
-    }
+    //     return false; 
+    // }
+
+    // public boolean bfs(int[] nums, int idx, boolean[] vis){
+    //     if(idx >= nums.length || idx < 0 || visited[idx]) return false;
+
+    //     if(idx == nums.length - 1) return true;
+
+    //     visited[idx] = true;
+
+        
+    // }
 
     public boolean canJump(int[] nums) {
         boolean[] visited = new boolean[nums.length];
-        return check(nums, 0, visited);
+        
+        Queue<Integer> items = new LinkedList<>();
+        int idx = 0;
+        visited[0] = true;
+        items.add(0);
+
+        while(!items.isEmpty()){
+
+            idx =  items.poll();
+            if(idx >= nums.length) continue;
+            visited[idx] = true;
+            if(idx == nums.length - 1) return true;
+
+            int maxItem = nums[idx];
+
+            visited[idx] = true;
+            for(int i = 1; i <= maxItem; i++){
+                int next = idx + i;
+                if (next < nums.length && !visited[next]) {
+                    visited[next] = true;
+                    items.add(idx + i);
+                }
+            }
+        }
+
+        return false;
+
     }
 }
