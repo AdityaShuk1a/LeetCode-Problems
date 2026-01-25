f9344db0ab93f3ebacb5d2ba5e80f706638166e7
Aditya Shukla <142577439+AdityaShuk1a@users.noreply.github.com>
2026-01-25 23:27:29 +0530

    Time: 11 ms (54.47%) | Memory: 46.5 MB (31.83%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,18 +1,35 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int maxLen = 0;
-        int[] freq = new int[26];
-        int maxO = 0;
+        int right = 0;
         int left = 0;
-        for(int i = 0; i < s.length(); i++){
-            maxO = Math.max(maxO, ++freq[s.charAt(i) - 'A']);
-            if((i - left - maxO + 1) > k){
-                freq[s.charAt(left) - 'A']--;
-                left++;
-            }
-            
+        int len = s.length();
+        int error = 0;
+        char key = s.charAt(0);
+        int maxLen = Integer.MIN_VALUE;
+        int[] character = new int[26];
 
-            maxLen = Math.max(maxLen, i- left + 1 );
+        while (right < len) {
+            character[s.charAt(right) - 'A']++;
+            if (character[key - 'A'] < character[s.charAt(right) - 'A']) {
+                key = s.charAt(right);
+            }
+
+            error = (right - left + 1) - character[key - 'A'];
+
+            while (error > k) {
+                character[s.charAt(left) - 'A']--;
+                left++;
+                for (int i = 0; i < 26; i++) {
+                    if (character[i] > character[key - 'A']) {
+                        key = (char) (i + 'A');
+                    }
+                }
+
+                error = (right - left + 1) - character[key - 'A'];
+            }
+
+            maxLen = Math.max(right - left + 1, maxLen);
+            right++;
         }
 
         return maxLen;
