447861683bc23ff86842ea6f5b647f92efebe53f
Aditya Shukla <142577439+AdityaShuk1a@users.noreply.github.com>
2026-04-02 22:10:19 +0530

    Time: 12 ms (57.26%) | Memory: 49.3 MB (46.69%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,50 +1,54 @@
 class Solution {
-    class Pair {
+    class Pair{
         int node;
-        int disst;
-        
-        public Pair(int node, int dist){
+        int dist;
+
+        Pair(int node, int dist){
             this.node = node;
-            this.disst = dist;
+            this.dist = dist;
         }
     }
-
     public int networkDelayTime(int[][] times, int n, int k) {
-        int INF = (int) 1e8;
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, INF);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.dist - b.dist);
+        int ans = -1;
         HashMap<Integer, ArrayList<Pair>> adj = new HashMap<>();
-        PriorityQueue<Pair> q = new PriorityQueue<>((a, b) -> a.disst - b.disst);
-        for(int i = 0; i < n; i++){
-            adj.put(i+1, new ArrayList<>());
-        }
-        for(int []i : times){
-            adj.computeIfAbsent(i[0], e -> new ArrayList<>()).add(new Pair(i[1], i[2]));
-        }
-        dist[k] = 0;
-        q.offer(new Pair(k, 0));
-        while(!q.isEmpty()){
-            Pair item = q.poll();
-            int node = item.node;
-            int w = item.disst;
 
-            if(w > dist[node]) continue;
-            for(Pair i: adj.get(node)){
-                int nei = i.node;
-                int wt = i.disst;
-                if(dist[node] + wt < dist[nei]){
-                    dist[nei] = dist[node] + wt;
-                    q.offer(i);
+        int[] dist = new int[n + 1];
+        for(int[] i: times){
+            adj.computeIfAbsent(i[0], l -> new ArrayList<>()).add(new Pair(i[1], i[2]));
+        }
+        for(int i = 0; i <= n; i++){
+            dist[i] = Integer.MAX_VALUE;
+        }
+        boolean[] vis = new boolean[n];
+        pq.add(new Pair(k, 0));
+        dist[k] = 0;
+
+        while(!pq.isEmpty()){
+            Pair p = pq.poll();
+            int node = p.node;
+            int dis = p.dist;
+            if (dis > dist[node]) continue;
+            if(!adj.containsKey(node)){
+                continue;
+            }
+            ArrayList<Pair> nei = adj.get(node);
+
+            for(Pair l: nei){
+                if(dist[l.node] > dis + l.dist){
+                    dist[l.node] = dis + l.dist;
+                    pq.add(new Pair(l.node, dist[l.node]));
                 }
             }
 
         }
-        int max = Integer.MIN_VALUE;
-        for(int i = 1; i < dist.length; i++){
-            if(dist[i] == (int)1e8) return -1;
-            max = Math.max(dist[i], max);
+
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) return -1;
+            ans = Math.max(ans, dist[i]);
         }
 
-        return max;
+        return ans;
+
     }
 }
